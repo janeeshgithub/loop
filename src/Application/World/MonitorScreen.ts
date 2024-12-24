7f6b3f0e4cc6803af51c1e7a20a2a911aafa34a1
JANEESH P <janeeshthelegend@gmail.com>
2024-12-24 11:04:18 +0530

    Update MonitorScreen.ts

--- src/Application/World/MonitorScreen.ts
+++ src/Application/World/MonitorScreen.ts
@@ -184,7 +184,7 @@ export default class MonitorScreen extends EventEmitter {
 
         // Set iframe attributes
         // PROD
-        iframe.src = 'http://192.168.168.20:8080/';
+        iframe.src = 'https://chris77.netlify.app';
         /**
          * Use dev server is query params are present
          *
