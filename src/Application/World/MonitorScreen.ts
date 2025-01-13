1e2e1bbb2f9842c5c608528a9c811c1fb8f43c0c
JANEESH P <janeeshthelegend@gmail.com>
2025-01-13 18:56:02 +0530

    Update MonitorScreen.ts

--- src/Application/World/MonitorScreen.ts
+++ src/Application/World/MonitorScreen.ts
@@ -184,7 +184,7 @@ export default class MonitorScreen extends EventEmitter {
 
         // Set iframe attributes
         // PROD
-        iframe.src = 'https://loop-indol-omega.vercel.app/';
+        iframe.src = 'https://portfolio-zeta-woad-94.vercel.app/';
         /**
          * Use dev server is query params are present
          *Hi
